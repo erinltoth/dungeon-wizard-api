d07f4ba5cac3c63d6b665a8426795cf2f36c0cf4
pnolan89 <petenol@gmail.com>
2019-03-01 21:37:57 +0000

    add campaign#index route

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,8 @@
 class CampaignsController < ApplicationController
+  def index
+    @campaigns = Campaign.all.order(created_at: :desc)
+    puts 'UsersController ---> INDEX'
+    puts @campaigns
+    render json: @campaigns
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
+  resources :users , only: [:index, :show]
+
+  resources :campaigns, only: [:index, :show, :create, :edit, :update]
 end
