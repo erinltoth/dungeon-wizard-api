8344efbe3ce6d95147e5554e69f0ed8a26748021
pnolan89 <petenol@gmail.com>
2019-03-01 23:21:31 +0000

    add #new and #create to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
     render json: @user
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:campaigns], notice: 'Account created!'
+    else
+      redirect_to [:campaigns], notice: 'Something went wrong'
+    end
+  end
 end
