234812c88c4359e47da33c96ed53684f5081d380
pnolan89 <petenol@gmail.com>
2019-03-01 19:23:06 +0000

    fix CORS issue

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,13 @@ module DungeonWizardApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    # config.middleware.use Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+    #   end
+    # end
+
     config.api_only = true
   end
 end
