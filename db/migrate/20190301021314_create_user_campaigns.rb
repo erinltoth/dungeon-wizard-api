955b37f263be82075c6b69d73f1a9a26dd7f567e
Erin Toth <erintoth@gmail.com>
2019-03-01 02:15:24 +0000

    Update UserCampaign model and migration

--- db/migrate/20190301021314_create_user_campaigns.rb
+++ db/migrate/20190301021314_create_user_campaigns.rb
@@ -1,6 +1,8 @@
 class CreateUserCampaigns < ActiveRecord::Migration[5.2]
   def change
     create_table :user_campaigns do |t|
+      t.references :user, foreign_key: true
+      t.references :campaign, foreign_key: true
 
       t.timestamps
     end
