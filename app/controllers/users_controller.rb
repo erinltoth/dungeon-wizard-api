37ddc7a67995b61aa59cb2ac04ffa82477df0a3e
pnolan89 <petenol@gmail.com>
2019-03-01 23:41:50 +0000

    add #update and user_params to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,26 @@ class UsersController < ApplicationController
       redirect_to [:campaigns], notice: 'Something went wrong'
     end
   end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      puts 'User update failed :('
+      redirect_to @user
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :playing_style,
+      :exp_level
+    )
+  end
 end
