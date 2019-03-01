2354b768e2a0adfd361e91c732f56944d41b051b
pnolan89 <petenol@gmail.com>
2019-03-01 01:13:18 +0000

    add some seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,26 @@ puts "Re-creating users..."
 
 User.destroy_all
 
-User.create!({ })
+User.create!({
+  name: 'Gandalf Grey',
+  email: 'mithran@dir.com',
+  password: 'youshallnotpass',
+  playing_style: "casual",
+  exp_level: "beginner"
+})
+
+User.create!({
+  name: 'Gimli Gloin',
+  email: 'thatstillonly@countsasone.com',
+  password: 'nobodytossesadwarf',
+  playing_style: "combat-focused",
+  exp_level: "beginner"
+})
+
+User.create!({
+  name: 'Jaime Lannister',
+  email: 'kingslayer@gmail.com',
+  password: 'thethingsidoforlove',
+  playing_style: "story-focused",
+  exp_level: "expert"
+})
