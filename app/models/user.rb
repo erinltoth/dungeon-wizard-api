79b7d9b94cf19e902f0acd7f6955ee09e627f245
pnolan89 <petenol@gmail.com>
2019-03-05 02:21:02 +0000

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :campaigns
   has_many :user_campaigns
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :playing_style, presence: true
+  validates :exp_level, presence: true
+
 end
