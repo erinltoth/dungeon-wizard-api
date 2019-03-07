9d89d33009fe64956a97423904ea1f1eaee4aca4
Erin Toth <erintoth@gmail.com>
2019-03-07 21:09:46 +0000

    Update join_requests_controller

--- app/controllers/join_requests_controller.rb
+++ app/controllers/join_requests_controller.rb
@@ -14,7 +14,7 @@ class JoinRequestsController < ApplicationController
     @join_request = JoinRequest.new(join_request_params)
 
     if @join_request.save
-      render json: @join_request.dm_confirm
+      render json: request:@join_request
     else
       redirect_to [:campaigns], notice: 'Something went wrong...'
     end
